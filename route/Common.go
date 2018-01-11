0137ec69d65b92007c0793e4036a135aaa793294
wangxinglong <wangxinglong@Iqidao.com>
2018-01-11 17:31:30 +0800

    common

--- route/Common.go
+++ route/Common.go
@@ -2,12 +2,11 @@ package route
 
 import (
 	"SGMS/domain/face/admin"
-	"path"
 
 	"github.com/kataras/iris"
 )
 
-const ADMIN_ROOT = "/admin001"
+// const ADMIN_ROOT = "/admin001"
 
 type Bread struct {
 	Url, Title string
@@ -22,23 +21,23 @@ type AdminData struct {
 	Breadcrumb []Bread
 }
 
-//压入面包屑
-func (this *AdminData) PushBreadAdmin(url, title string) *AdminData {
-	this.Breadcrumb = append(this.Breadcrumb, Bread{Url: ADMIN_ROOT + url, Title: title})
-	return this
-}
+// //压入面包屑
+// func (this *AdminData) PushBreadAdmin(url, title string) *AdminData {
+// 	this.Breadcrumb = append(this.Breadcrumb, Bread{Url: ADMIN_ROOT + url, Title: title})
+// 	return this
+// }
 
 // type PageData struct {
 // 	route.PaginationParam
 // 	AdminData
 // }
 
-func GetRequestPath(ctx *iris.Context) string {
-	return ctx.RequestPath(true)[len(ADMIN_ROOT):]
-}
+// func GetRequestPath(ctx *iris.Context) string {
+// 	return ctx.RequestPath(true)[len(ADMIN_ROOT):]
+// }
 
 func Redirect(ctx *iris.Context, p string) {
-	ctx.Redirect(path.Join(ADMIN_ROOT, p), iris.StatusOK)
+	ctx.Redirect(p, iris.StatusOK)
 }
 
 // func Ok(ctx *iris.Context, data ...interface{}) {
