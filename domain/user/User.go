31000b5897e6352b99a11aca0434b67e52e30792
wangxinglong <wangxinglong@Iqidao.com>
2018-02-02 18:06:39 +0800

    modify

--- domain/user/User.go
+++ domain/user/User.go
@@ -5,7 +5,6 @@ import (
 	"SGMS/domain/exception"
 	"SGMS/domain/face"
 	"SGMS/domain/table"
-	"SGMS/domain/util"
 )
 
 type User struct {
@@ -20,7 +19,8 @@ func (this *User) Signin(param face.UserSigninParam) table.User {
 	// if c <= 0 {
 	// 	return table.User{}
 	// }
-	psql := "select * from User where professionNo = :Key and (password = :Password or password = " + util.Md5(param.Password) + ")"
+	// psql := "select * from User where professionNo = :Key and (password = :Password or password = " + util.Md5(param.Password) + ")"
+	psql := "select * from User where phone = :Key and password = :Password "
 	u := []table.User{}
 	_, err := mysql.Select(&u, psql, param)
 	exception.CheckMysqlError(err)
